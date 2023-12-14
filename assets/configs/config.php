b49bb025af959ca8f9db5a2cd9d2dd4585597473
oscareduardomc <108117209+oscareduardomc@users.noreply.github.com>
2023-12-13 22:30:58 -0600

    Update config.php

--- assets/configs/config.php
+++ assets/configs/config.php
@@ -1,8 +1,1 @@
-<?php
-  define("DB_HOST", "localhost");
-  define("DB_USER", "u281201638_dcom");
-  define("DB_PASSWORD", "Dcom2022");
-  define("DB_DATABASE", "u281201638_proyectos");
 
-  $conex = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-?>
